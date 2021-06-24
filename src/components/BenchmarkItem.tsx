5d319a33fcc1146bf056da46bd902bfc4195ff8f
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-24 23:00:51 +0200

    Add: benchmark item

---
+++ src/components/BenchmarkItem.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+interface BenchmarkItemProps {
+Name: string
+}
+
+interface ListItemBlockProps {
+  Size: number
+}
+
+const ListItemWrapper = styled.div`
+display:flex
+`
+const ListItemBlock = styled.div<ListItemBlockProps>`
+flex: ${({Size})=> Size? Size:'1' }
+`
+export const BenchmarkItem = (props: BenchmarkItemProps) => {
+  return (
+    <ListItemWrapper>
+      <ListItemBlock Size={2}>
+        {({Name})=> Name ? Name : ''}
+      </ListItemBlock>
+      <ListItemBlock Size={1}>
+        <button>Start benchmark</button>
+      </ListItemBlock>
+    </ListItemWrapper>
+  )
+}
