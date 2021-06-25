77cf6449718b0e3547dfdc4ad9f129df8e38d3a5
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-25 21:10:21 +0200

    Add: Listing Benchmark items

--- src/components/BenchmarkList.tsx
+++ src/components/BenchmarkList.tsx
@@ -1,10 +1,10 @@
 import { BenchmarkItem } from './BenchmarkItem'
 
 export const BenchmarkList = () => {
-  
+  const names: string[] = ["Bench1","Bench2","Bench3","Bench4"]
   return (
     <div>
-
+      {names.map(element => <BenchmarkItem Name={element}/>)}
     </div>
   )
 }
